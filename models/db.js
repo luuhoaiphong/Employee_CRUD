c6ddfd3e642b4a78a31a0adced55bc484122df4c
luuhoaiphong <luuhoaiphong147@gmail.com>
2022-10-19 16:17:34 +0700

    url

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //const url = "mongodb://localhost:27017/EmployeeDB";
 //const url = "mongodb+srv://tmtuan:**************@cluster0.zfovn.mongodb.net/test"
-const url = "mongodb+srv://phonglhgcc:Phong@123@atlascluster.d8z7xmo.mongodb.net/?retryWrites=true&w=majority";
+const url = "mongodb+srv://phonglhgcc:tNsXxLpIOMvKzZUR@atlascluster.d8z7xmo.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(url,{useNewUrlParser:true},(err) => {
     if(!err){ console.log("MongoDB Connection Succeeded");}
     else{
